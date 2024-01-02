295573eb2fc9b9d180928f98d2c8e27f030d8f47
s582015 <kateryna.tishchenko@student.htw-berlin.de>
2024-01-02 18:07:47 +0100

    save changes

--- tests/integration/game_score_test.py
+++ tests/integration/game_score_test.py
@@ -11,7 +11,31 @@ def test_game_score_game_not_found(client):
 
     response = client.post("/api/start/", json=invitation_data)
     response_json = response.json
-    game_id = 12345 #invalid game_id
+    game_id = 12345  # invalid game_id
+    token = response_json.get('token', None)
+    headers = {"Authorization": token}
+
+    # Calling 'scored' endpoint with not existing game id
+    response = client.post(f"/api/game/{game_id}/scored", headers=headers, json={"points": 10})
+
+    assert response.status_code == 404
+    assert response.content_type == "application/json"
+
+
+def test_game_score_update_successful(client):
+    """
+    WHEN calling the 'scored' endpoint with valid data, the score should be successfully assigned
+    THEN check if the response status code is 200
+    """
+    invitation_data = {"username": "tim_user",
+                       "skin": 9,
+                       "rounds": 2,
+                       "lapTime": 2,
+                       "amountUsers": 2}
+
+    response = client.post("/api/start/", json=invitation_data)
+    response_json = response.json
+    game_id = response_json.get('game_id')
     token = response_json.get('token', None)
     headers = {"Authorization": token}
 
@@ -20,5 +44,5 @@ def test_game_score_game_not_found(client):
     # Calling 'scored' endpoint with not existing game id
     response = client.post(f"/api/game/{game_id}/scored", headers=headers, json={"points": 10})
 
-    assert response.status_code == 404
+    assert response.status_code == 200
     assert response.content_type == "application/json"
