68569e1703ef7fc9886a11819ba4e217f94dd97e
s582015 <kateryna.tishchenko@student.htw-berlin.de>
2024-01-02 17:45:02 +0100

    save changes

--- tests/integration/game_status_test.py
+++ tests/integration/game_status_test.py
@@ -1,4 +1,3 @@
-
 # Tests for '/api/game/{gameId}/status' endpoint
 def test_get_game_status_authorisation_error(client):
     """
@@ -63,4 +62,29 @@ def test_get_game_status_game_is_not_started(client):
 
     response = client.get(f"/api/game/{game_id}/status", headers=headers)
     assert response.status_code == 201
+    assert response.content_type == "application/json"
+
+
+def test_game_score_game_not_found(client):
+    """
+    WHEN calling the 'scored' endpoint with invalid game id
+    THEN check if the response status code is 404
+    """
+    invitation_data = {
+        "username": "tim_user",
+        "skin": 9,
+        "rounds": 2,
+        "lapTime": 2,
+        "amountUsers": 2
+    }
+
+    response_json = client.post("/start/", json=invitation_data)
+    game_id = 12345
+    token = response_json.get('token', None)
+    headers = {"Authorization": token}
+
+    # Calling 'scored' endpoint with not existing game id
+    response = client.post(f"/game/{game_id}/scored", headers=headers, json={"points": 10})
+
+    assert response.status_code == 404
     assert response.content_type == "application/json"
